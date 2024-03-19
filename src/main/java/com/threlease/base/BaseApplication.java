34aa2b4f1aa229682226465eba8682807fc94313
th-release <cth.release@gmail.com>
2024-03-20 08:31:09 +0900

    feat: EnabledScheduling

--- src/main/java/com/threlease/base/BaseApplication.java
+++ src/main/java/com/threlease/base/BaseApplication.java
@@ -3,6 +3,7 @@ package com.threlease.base;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.io.File;
 import java.io.IOException;
@@ -11,6 +12,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 @EnableJpaRepositories
+@EnableScheduling
 @SpringBootApplication
 public class BaseApplication {
 	public static void main(String[] args) {
