a627b656b12669ced33ba4bcb66775076d6e8295
th-release <cth.release@gmail.com>
2024-03-21 08:42:01 +0900

    feat: Added create, reset, restart, getKeypair

--- src/main/java/com/threlease/base/functions/invite/InviteController.java
+++ src/main/java/com/threlease/base/functions/invite/InviteController.java
@@ -6,10 +6,12 @@ import com.threlease.base.entites.InviteEntity;
 import com.threlease.base.functions.auth.AuthService;
 import com.threlease.base.functions.aws.ManageInstanceService;
 import com.threlease.base.functions.invite.dto.CreateInvite;
+import com.threlease.base.utils.Failable;
 import com.threlease.base.utils.responses.BasicResponse;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import software.amazon.awssdk.services.ec2.Ec2Client;
 
 import java.util.Optional;
 
@@ -58,38 +60,163 @@ public class InviteController {
             Optional<AuthEntity> user = authService.findOneByToken(token);
 
             if (user.isPresent()) {
-                Optional<InstanceEntity> instance = manageInstanceService.findOneByUuid(dto.getId());
-                if (instance.isEmpty()) {
+                    Optional<InstanceEntity> instance = manageInstanceService.findOneByUuid(dto.getId());
+                    if (instance.isEmpty()) {
+                            BasicResponse response = BasicResponse.builder()
+                                    .success(false)
+                                    .message(Optional.of("NOT FOUND INSTANCE"))
+                                    .data(Optional.empty())
+                                    .build();
+
+                            return ResponseEntity.status(404).body(response);
+                    }
+
+                    InviteEntity invite = InviteEntity.builder()
+                            .instance(instance.get())
+                            .build();
+
+                    inviteService.inviteSave(invite);
+
+                    BasicResponse response = BasicResponse.builder()
+                            .success(true)
+                            .message(Optional.empty())
+                            .data(Optional.of(invite))
+                            .build();
+
+                    return ResponseEntity.status(201).body(response);
+            } else {
                     BasicResponse response = BasicResponse.builder()
                             .success(false)
-                            .message(Optional.of("NOT FOUND INSTANCE"))
+                            .message(Optional.of("INVALID SESSION"))
                             .data(Optional.empty())
                             .build();
 
-                    return ResponseEntity.status(404).body(response);
-                }
+                    return ResponseEntity.status(403).body(response);
+            }
+    }
 
-                InviteEntity invite = InviteEntity.builder()
-                        .instance(instance.get())
-                        .build();
+    @PostMapping("/{id}/restart")
+    public ResponseEntity<?> restartInviteInstance(
+            @PathVariable("id") String id
+    ) {
+        Ec2Client ec2Client = manageInstanceService.getEc2Client();
+        Optional<InviteEntity> invite = inviteService.findOneByUuid(id);
+        if (invite.isEmpty()) {
+            BasicResponse response = BasicResponse.builder()
+                    .success(false)
+                    .message(Optional.of("INVALID INVITE"))
+                    .data(Optional.empty())
+                    .build();
 
-                inviteService.inviteSave(invite);
+            return ResponseEntity.status(403).body(response);
+        }
 
-                BasicResponse response = BasicResponse.builder()
-                        .success(true)
-                        .message(Optional.empty())
-                        .data(Optional.of(invite))
-                        .build();
+        Failable<Boolean, String> restartRequest = manageInstanceService.restartInstance(
+                ec2Client,
+                invite.get().getInstance()
+        );
+        ec2Client.close();
 
-                return ResponseEntity.status(201).body(response);
-            } else {
+        if (restartRequest.isError()) {
+            BasicResponse response = BasicResponse.builder()
+                    .success(false)
+                    .message(Optional.of(restartRequest.getError()))
+                    .data(Optional.empty())
+                    .build();
+
+            return ResponseEntity.status(500).body(response);
+        }
+
+        BasicResponse response = BasicResponse.builder()
+                .success(true)
+                .message(Optional.empty())
+                .data(Optional.empty())
+                .build();
+
+        return ResponseEntity.status(200).body(response);
+    }
+
+    @PostMapping("/{id}/reset")
+    public ResponseEntity<?> resetInviteInstance(
+            @PathVariable("id") String id
+    ) {
+        Ec2Client ec2Client = manageInstanceService.getEc2Client();
+        Optional<InviteEntity> invite = inviteService.findOneByUuid(id);
+        if (invite.isEmpty()) {
                 BasicResponse response = BasicResponse.builder()
                         .success(false)
-                        .message(Optional.of("INVALID SESSION"))
+                        .message(Optional.of("INVALID INVITE"))
                         .data(Optional.empty())
                         .build();
 
                 return ResponseEntity.status(403).body(response);
-            }
+        }
+
+        Failable<Boolean, String> resetRequest = manageInstanceService.resetInstance(
+                ec2Client,
+                invite.get().getInstance()
+        );
+
+        ec2Client.close();
+
+        if (resetRequest.isError()) {
+            BasicResponse response = BasicResponse.builder()
+                    .success(false)
+                    .message(Optional.of(resetRequest.getError()))
+                    .data(Optional.empty())
+                    .build();
+
+            return ResponseEntity.status(500).body(response);
+        }
+
+        BasicResponse response = BasicResponse.builder()
+                .success(true)
+                .message(Optional.empty())
+                .data(Optional.empty())
+                .build();
+
+        return ResponseEntity.status(200).body(response);
+    }
+
+    @GetMapping("/{id}/keypair")
+    public ResponseEntity<?> getKeypairInviteInstance(
+            @PathVariable("id") String id
+    ) {
+        Ec2Client ec2Client = manageInstanceService.getEc2Client();
+        Optional<InviteEntity> invite = inviteService.findOneByUuid(id);
+        if (invite.isEmpty()) {
+            BasicResponse response = BasicResponse.builder()
+                    .success(false)
+                    .message(Optional.of("INVALID INVITE"))
+                    .data(Optional.empty())
+                    .build();
+
+            return ResponseEntity.status(403).body(response);
+        }
+
+        Failable<Boolean, String> resetRequest = manageInstanceService.resetInstance(
+                ec2Client,
+                invite.get().getInstance()
+        );
+
+        ec2Client.close();
+
+        if (resetRequest.isError()) {
+            BasicResponse response = BasicResponse.builder()
+                    .success(false)
+                    .message(Optional.of(resetRequest.getError()))
+                    .data(Optional.empty())
+                    .build();
+
+            return ResponseEntity.status(500).body(response);
+        }
+
+        BasicResponse response = BasicResponse.builder()
+                .success(true)
+                .message(Optional.empty())
+                .data(Optional.of(invite.get().getInstance().getKeypair()))
+                .build();
+
+        return ResponseEntity.status(200).body(response);
     }
 }
--- src/main/java/com/threlease/base/functions/invite/InviteService.java
+++ src/main/java/com/threlease/base/functions/invite/InviteService.java
@@ -1,15 +1,20 @@
 package com.threlease.base.functions.invite;
 
 import com.threlease.base.entites.InviteEntity;
+import com.threlease.base.functions.aws.ManageInstanceService;
 import com.threlease.base.repositories.InviteRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Service
 public class InviteService {
     private final InviteRepository inviteRepository;
+    private final ManageInstanceService manageInstanceService;
 
-    public InviteService(InviteRepository inviteRepository) {
+    public InviteService(InviteRepository inviteRepository, ManageInstanceService manageInstanceService) {
         this.inviteRepository = inviteRepository;
+        this.manageInstanceService = manageInstanceService;
     }
 
     public void inviteSave(InviteEntity data) {
