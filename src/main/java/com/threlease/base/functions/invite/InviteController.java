b25a05175d9ec463d554d527ee61b3d52148364b
th-release <cth.release@gmail.com>
2024-03-20 17:21:53 +0900

    feat: Added CreateInvite

--- src/main/java/com/threlease/base/functions/invite/InviteController.java
+++ src/main/java/com/threlease/base/functions/invite/InviteController.java
@@ -1,12 +1,15 @@
 package com.threlease.base.functions.invite;
 
+import com.threlease.base.entites.AuthEntity;
+import com.threlease.base.entites.InstanceEntity;
 import com.threlease.base.entites.InviteEntity;
+import com.threlease.base.functions.auth.AuthService;
+import com.threlease.base.functions.aws.ManageInstanceService;
+import com.threlease.base.functions.invite.dto.CreateInvite;
 import com.threlease.base.utils.responses.BasicResponse;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -14,9 +17,13 @@ import java.util.Optional;
 @RequestMapping("/invite")
 public class InviteController {
     private final InviteService inviteService;
+    private final AuthService authService;
+    private final ManageInstanceService manageInstanceService;
 
-    public InviteController(InviteService inviteService) {
+    public InviteController(InviteService inviteService, AuthService authService, ManageInstanceService manageInstanceService) {
         this.inviteService = inviteService;
+        this.authService = authService;
+        this.manageInstanceService = manageInstanceService;
     }
 
     @GetMapping("/{uuid}")
@@ -26,20 +33,63 @@ public class InviteController {
         Optional<InviteEntity> invite = inviteService.findOneByUuid(uuid);
         if (invite.isEmpty()) {
             BasicResponse response = BasicResponse.builder()
-                    .success(false)
-                    .message(Optional.of("NOT FOUND INVITE"))
-                    .data(Optional.empty())
-                    .build();
+                .success(false)
+                .message(Optional.of("NOT FOUND INVITE"))
+                .data(Optional.empty())
+                .build();
 
             return ResponseEntity.status(404).body(response);
         }
 
         BasicResponse response = BasicResponse.builder()
-                .success(true)
-                .message(Optional.empty())
-                .data(Optional.of(invite.get()))
-                .build();
+            .success(true)
+            .message(Optional.empty())
+            .data(Optional.of(invite.get()))
+            .build();
 
         return ResponseEntity.status(200).body(response);
+    }
+
+    @PostMapping("/")
+    private ResponseEntity<?> createInvite(
+            @ModelAttribute @Valid CreateInvite dto,
+            @RequestHeader("Authorization") String token
+    ) {
+            Optional<AuthEntity> user = authService.findOneByToken(token);
+
+            if (user.isPresent()) {
+                Optional<InstanceEntity> instance = manageInstanceService.findOneByUuid(dto.getId());
+                if (instance.isEmpty()) {
+                    BasicResponse response = BasicResponse.builder()
+                            .success(false)
+                            .message(Optional.of("NOT FOUND INSTANCE"))
+                            .data(Optional.empty())
+                            .build();
+
+                    return ResponseEntity.status(404).body(response);
+                }
+
+                InviteEntity invite = InviteEntity.builder()
+                        .instance(instance.get())
+                        .build();
+
+                inviteService.inviteSave(invite);
+
+                BasicResponse response = BasicResponse.builder()
+                        .success(true)
+                        .message(Optional.empty())
+                        .data(Optional.of(invite))
+                        .build();
+
+                return ResponseEntity.status(201).body(response);
+            } else {
+                BasicResponse response = BasicResponse.builder()
+                        .success(false)
+                        .message(Optional.of("INVALID SESSION"))
+                        .data(Optional.empty())
+                        .build();
+
+                return ResponseEntity.status(403).body(response);
+            }
     }
 }
--- src/main/java/com/threlease/base/functions/invite/dto/CreateInvite.java
+++ src/main/java/com/threlease/base/functions/invite/dto/CreateInvite.java
@@ -1,2 +1,10 @@
-package com.threlease.base.functions.invite.dto;public class createInvite {
+package com.threlease.base.functions.invite.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Data;
+
+@Data
+public class CreateInvite {
+    @NotEmpty
+    private String id;
 }
